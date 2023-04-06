4ff2b6dd2b571d21440e7b113157329eb9fe534a
Mahire <mhr-isyan@msn.com>
2023-04-06 21:49:27 +0200

    ProductReviewsPage

--- src/main/java/maganto/frontendpages/NewsLetterSubscriptionsPage.java
+++ src/main/java/maganto/frontendpages/NewsLetterSubscriptionsPage.java
@@ -4,25 +4,30 @@ import maganto.utility.TestUtility;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class NewsLetterSubscriptionsPage {
 
     WebDriver driver;
 
-    public NewsLetterSubscriptionsPage(WebDriver driver, TestUtility utility, WebElement newsletterLink, WebElement generalSubscriptionText) {
-        this.driver = driver;
-        this.utility = utility;
-        this.newsletterLink = newsletterLink;
-        this.generalSubscriptionText = generalSubscriptionText;
-    }
 
     TestUtility utility;
 
     //Newsletter Subscriptions
     @FindBy(xpath = "//amyProductReviews[text()='Newsletter Subscriptions']")
     WebElement newsletterLink;
+
     @FindBy(xpath="//label[contains(text(),'General Subscription']")
     WebElement generalSubscriptionText;
 
+    public NewsLetterSubscriptionsPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+        utility= new TestUtility(driver);
+    }
+    public boolean verifyViewNewsletterContent(){
+        utility.waitForElementPresent(generalSubscriptionText);
+        return generalSubscriptionText.isDisplayed();
+    }
 
 }

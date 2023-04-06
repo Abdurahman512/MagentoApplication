f240e1858cb4c705febc5302f9719594a33996d5
Mahire <mhr-isyan@msn.com>
2023-04-06 02:12:27 +0200

    ProductReviewsPage

--- src/main/java/maganto/frontendpages/ProductReviewsPage.java
+++ src/main/java/maganto/frontendpages/ProductReviewsPage.java
@@ -1,4 +1,44 @@
 package maganto.frontendpages;
 
+import maganto.utility.TestUtility;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+
 public class ProductReviewsPage {
+
+ //account link
+ @FindBy(xpath = "//span[@class='label' and text()='Account']")
+ WebElement accountLink;
+
+
+   //my product reviews
+    @FindBy(xpath ="//strong[contains(text(),'My Product Reviews')]")
+    WebElement myProductReviews;
+
+    //Newsletter Subscriptions
+    @FindBy(xpath = "//amyProductReviews[text()='Newsletter Subscriptions']")
+    WebElement newsletterLink;
+
+
+    WebDriver driver;
+    TestUtility utility;
+
+ public void myProductReviews() {
+  utility.waitForElementPresent(accountLink);
+  accountLink.click();
+utility.waitForElementPresent(myProductReviews);
+  myProductReviews.click();
+ }
+
+ public void clickOnNewsletterLink() {
+  utility.waitForElementPresent(newsletterLink);
+  newsletterLink.click();
+ }
+
+
+
+
+
 }

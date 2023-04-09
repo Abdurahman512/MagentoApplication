49a22989ebc0fbd0eaf57663d4b5d92c97eac12f
Mahire <mhr-isyan@msn.com>
2023-04-10 00:51:29 +0200

    storeview

--- src/main/java/maganto/backendpages/storepages/StoreViewPage.java
+++ src/main/java/maganto/backendpages/storepages/StoreViewPage.java
@@ -1,6 +1,7 @@
 package maganto.backendpages.storepages;
 
 import maganto.utility.TestUtility;
+import org.apache.tools.ant.types.selectors.SelectSelector;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -11,9 +12,9 @@ public class StoreViewPage {
     //Store Manager can create a store view
     WebDriver driver;
     TestUtility testUtility;
-    String name;
-    String code;
-    String sortOrder;
+    String storeName;
+    String storeCode;
+    String storeSortOrder;
 
     public StoreViewPage(WebDriver driver){
         this.driver=driver;
@@ -34,13 +35,13 @@ public class StoreViewPage {
     @FindBy(xpath = "//select[@id='store_group_id']")
     WebElement storeDropDown;
     @FindBy(id = "store_name")
-    WebElement nameField;
+    WebElement storeNameField;
     @FindBy(id = "store_code")
-    WebElement codeField;
+    WebElement storeCodeField;
     @FindBy(id = "store_is_active")
     WebElement statusDropDown;
     @FindBy(id = "store_sort_order")
-    WebElement sortOrderField;
+    WebElement storeSortOrderField;
     @FindBy(xpath = "//body[1]/div[1]/div[3]/div[1]/div[2]/p[1]/button[3]/span[1]/span[1]/span[1]")
     WebElement saveStoreViewLink;
     @FindBy(xpath ="//span[contains(text(),'The store view has been saved')]")
@@ -49,11 +50,32 @@ public class StoreViewPage {
 
 
 
-    public void createStoreView(){
+    public void createStoreView(String storeName, String storeCode, String storeSortOrder){
+        testUtility.waitForElementPresent(createStoreViewLink);
+        createStoreViewLink.click();
+        testUtility.waitForElementPresent(storeNameField);
+        storeCodeField.sendKeys(storeName);
+        testUtility.waitForElementPresent(storeCodeField);
+        storeCodeField.sendKeys(storeCode);
+        testUtility.waitForElementPresent(storeSortOrderField);
+        storeSortOrderField.sendKeys(storeSortOrder);
+        testUtility.waitForElementPresent(saveStoreViewLink);
+        saveStoreViewLink.click();
+
+
+
+    }
+    public boolean verifyStoreViewSaved(){
+        testUtility.waitForElementPresent(successMessage);
+        if(successMessage.isDisplayed())
+            return true;
+        else return false;
+    }
+
 
 
     }
 
 
 
-}
+

cc306d6bf38784b86a1a982fb7b35a88d1f0304f
Abdurahman512 <oabdurahman512@gmail.com>
2023-04-27 17:45:15 +0200

    Marketing Module Donee

--- src/test/java/regressiontestsuit/cucumber/CucumberTestRunner.java
+++ src/test/java/regressiontestsuit/cucumber/CucumberTestRunner.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.WebDriver;
 
                 "junit:target/cucumber-results.xml"},// for test report
         features="src/test/resources/features", // feature file location
+
         tags = "@CatalogPriceRule"
         //tags = "@updateexistingCatalogPriceRule"
         //tags = "@searchByIdAndRule"
